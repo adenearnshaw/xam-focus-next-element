b66b7c1d716987cb1bd5b9c35c2a059a106452bd
Aden Earnshaw <aden.earnshaw@positive-solutions.co.uk>
2021-02-07 17:15:33 +0000

    Convert behavior to attached properties

---
+++ NextFormFieldSample.Forms/NextFormFieldSample.Forms/AttachedProperties/SetFocusOnEntryCompletedAttachedProperty.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NextFormFieldSample.Forms.AttachedProperties
+{
+    public class SetFocusOnEntryCompleted
+    {
+
+        public static readonly BindableProperty TargetElementProperty =
+            BindableProperty.CreateAttached("TargetElement", typeof(VisualElement), typeof(SetFocusOnEntryCompleted), default(VisualElement), propertyChanged: OnTargetElementChanged);
+
+        public static VisualElement GetTargetElement(BindableObject view)
+        {
+            return (VisualElement)view.GetValue(TargetElementProperty);
+        }
+
+        public static void SetTargetElement(BindableObject view, VisualElement value)
+        {
+            view.SetValue(TargetElementProperty, value);
+        }
+
+        private static void OnTargetElementChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var entry = bindable as Entry;
+
+            if (entry == null)
+                return;
+
+            entry.Completed += (s,e) => GetTargetElement(entry)?.Focus();
+        }        
+    }
+}
